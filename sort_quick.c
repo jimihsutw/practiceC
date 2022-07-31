ff232a21bffb58650ae0111531db3c9e94624154
jimihsutw <jimihsu@yahoo.com.tw>
2022-07-31 17:12:39 +0800

    quick sort

--- sort_quick.c
+++ sort_quick.c
@@ -3,43 +3,55 @@
 
 int main() {
     int data[] = {35, 10, 42, 3, 79, 12, 62, 18, 51, 23} ;
-    int k, i=0, j=SIZE-1 ;
-    int s, e ;
 
-    sort(data, 0, SIZE) ;
+    sort(data, 0, SIZE-1) ;
     dump(data, SIZE) ;
 
     return 0;
 }
 
-int sort(int data[], int start, int end) {
-    int temp;
+void sort(int data[], int start, int end) {
+    int temp, flag;
     int k = start ;
+    int last = end ;
     while (start<end) {
+        flag=0;
         dump(data, SIZE) ;
-        for (int i=start;i<end;i++) {
+        for (int i=start;i<=end;i++) {
             if (data[k] < data[i]) {
                 start = i;
+                flag+=1;
                 break ;
             }
         }
 
-        for (int i=end-1;i>k;i--) {
+        for (int i=end;i>k;i--) {
             if (data[k]>data[i]) {
                 end = i;
+                flag+=2;
                 break;
             }
         }
 
         if (start<end) {
+            if (data[start]<data[end]) {
+                return ;
+            }
+
             temp = data[start];
             data[start] = data[end] ;
             data[end] = temp ;
         } else if (start>=end) {
+            if (data[k]<data[end]) {
+                return ;
+            }
             temp=data[k];
             data[k]=data[end];
             data[end]=temp;
-            return end ;
+
+            sort(data, k, end-1) ;
+            sort(data, end+1, last);
+            return ;
         }
     }
 }

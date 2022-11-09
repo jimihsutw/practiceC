7a9d490cd71427263baf3bdc9edb2e6310c466c8
jimihsutw <jimihsu@yahoo.com.tw>
2022-11-09 22:05:49 +0800

    登入

--- kattis/Login.cpp
+++ kattis/Login.cpp
@@ -1,43 +1,35 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<map>
 using namespace std;
 typedef long long ll;
 
 int main() {
     int  n, m;
     cin>>n>>m;
+    string name, pwd;
 
-    string name[n][2];
+    map<string, string> np;
     char ret[m]={0};
 
     for(int i=0;i<n;i++) {
-        cin>>name[i][0]>>name[i][1];
+        cin>>name>>pwd;
+        np[name]=pwd;
     }
 
     int cmd;
-    string nm, pwd;
     for(int i=0;i<m;i++) {
-        cin>>cmd>>nm>>pwd;
+        cin>>cmd>>name>>pwd;
         if (cmd==1) { // login
-            for (int j=0;j<n;j++) {
-                if (name[j][0]==nm)  {
-                    if (name[j][1]==pwd) {
-                        ret[i]='1';
-                    } else {
-                        ret[i]='2';
-                    }
-                    break;
-                }
+            if (np[name]==pwd)  {
+                ret[i]='1';
+            } else {
+                ret[i]='2';
             }
         } else { // modify
-            for (int j=0;j<n;j++) {
-                if (name[j][0]==nm)  {
-                    name[j][1]=pwd;
-                    ret[i]='1';
-                    break;
-                }
-            }
+            np[name]=pwd;
+            ret[i]='1';
         }
     }
 
